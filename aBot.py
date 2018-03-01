eb24facbfd627eefae0e052cc32cadf5b60389c3
zirr <31447312+zirriously@users.noreply.github.com>
2018-03-01 11:41:02 +0100

    maybe final version, changeable channel id and token w external loading

--- aBot.py
+++ aBot.py
@@ -3,11 +3,21 @@ import asyncio
 import os
 
 token = open("token.txt", "r").read()
+channelID = open("channelid.txt", "r").read()
 
-client = discord.Client
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("logged in...")
+    print("Logged in with " + token + ", listening for messages on " + channelID)
+
+@client.event
+async def on_message(message):
+    if message.channel.id == channelID and message.content.startswith("."):
+        await message.add_reaction("👍")
+        await message.add_reaction("👎")
+        await message.add_reaction("🔴")
+        print("Added reactions to - " + message.content)
+
 
 client.run(token)
